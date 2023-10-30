a875d52ff6c852aa3e32f076111d088b1bb8ed32
Lu Matias <146031994+luhmaa@users.noreply.github.com>
2023-10-30 09:20:49 -0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="menu.css">
+</head>
+<body>
+    <div class="menu">
+        <ul class="menu-list">
+            <li><a href="#">Hoje</a></li>
+            <li><a href="#">Semana</a></li>
+            <li><a href="#">Mês</a></li>
+        </ul>
+        <div class="menu-btn" onclick="toggleMenu()">&#9776;</div>
+        <ul class="submenu">
+            <li><a href="#">Concluído</a></li>
+            <li><a href="#">Pendente</a></li>
+            <li><a href="#">Avisos</a></li>
+        </ul>
+    </div>
+    <div class="login">
+        <a href="login.php">Login</a>
+    </div>
+</body>
+</html>
