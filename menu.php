a89ab0d9dcb3a3cef37cd3b075bab644a0315dd7
Lu Matias <146031994+luhmaa@users.noreply.github.com>
2023-10-31 10:02:20 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,12 +4,7 @@
     <link rel="stylesheet" type="text/css" href="menu.css">
 </head>
 <body>
-    <div class="menu">
-        <ul class="menu-list">
-            <li><a href="#">Hoje</a></li>
-            <li><a href="#">Semana</a></li>
-            <li><a href="#">Mês</a></li>
-        </ul>
+    
         <div class="menu-btn" onclick="toggleMenu()">&#9776;</div>
         <ul class="submenu">
             <li><a href="#">Concluído</a></li>
@@ -17,8 +12,8 @@
             <li><a href="#">Avisos</a></li>
         </ul>
     </div>
-    <div class="login">
-        <a href="login.php">Login</a>
+    <div class="adicionarTarefa">
+        <a href="adicionarTarefa.php">Adicionar Tarefa</a>
     </div>
 </body>
 </html>
